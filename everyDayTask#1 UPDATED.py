c1bb9d9f9766330345610499180e49321b8ac73c
Mohammad Mahdi Takfallah <mmtmmt945@gmail.com>
2024-08-15 15:05:08 +0800

    updated task#1

--- everyDayTask#1 UPDATED.py
+++ everyDayTask#1 UPDATED.py
@@ -11,15 +11,22 @@ while keepGettingNum == True:
     else :
         listOfNum.append(int(number))
 
-
 maxNum=int(input("now please enter the max number you want to see ."))
 minNum=int(input("now please enter the min number you want to see ."))
 
 resultList=[]
 
-(resultList.append(x) for x in listOfNum if x<=maxNum and x>=minNum)
+A=[x for x in listOfNum if x<=maxNum and x>=minNum]
+for i in A:
+    resultList.append(i)
 
 print(resultList)
+
+
+
+
+
+
 
 
 
