fb43b7a04ac7177c5562d69b8dc27d808d9673e3
Mohammad Mahdi Takfallah <mmtmmt945@gmail.com>
2024-08-15 14:26:05 +0800

    Create everyDayTask#1 UPDATED.py
    task1 updated

---
+++ everyDayTask#1 UPDATED.py
@@ -0,0 +1,26 @@
+
+print("please enter your list of nums and when you are done pls enter (done) . ")
+
+listOfNum=[]
+keepGettingNum=True
+while keepGettingNum == True:
+
+    number=input()
+    if number == 'done':
+        keepGettingNum=False
+    else :
+        listOfNum.append(int(number))
+
+
+maxNum=int(input("now please enter the max number you want to see ."))
+minNum=int(input("now please enter the min number you want to see ."))
+
+resultList=[]
+
+(resultList.append(x) for x in listOfNum if x<=maxNum and x>=minNum)
+
+print(resultList)
+
+
+
+
